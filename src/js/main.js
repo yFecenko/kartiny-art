8502cac1629192d11a6ab1c1f44c345748fa8d09
Yana <nanaha28@gmail.com>
2023-08-13 19:32:56 +0300

    loading hidden elements

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,5 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 
 });
