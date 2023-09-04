a79d0bda775efb9d820a5e575d9d5772b3d596bf
Yana <nanaha28@gmail.com>
2023-09-04 22:17:25 +0300

    drag & drop

--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -39,6 +39,7 @@ const scrolling = (upSelector) => {
 
                 if (r != widthTop + toBlock) {
                     requestAnimationFrame(step);
+                    
                 } else {
                     location.hash = hash;
                 }
